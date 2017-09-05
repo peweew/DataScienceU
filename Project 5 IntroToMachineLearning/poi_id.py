1e3533c2f9cec826f0a9bf36097f49170116055b
pewee <pengwwan@gmail.com>
2017-09-05 11:44:33 +0800

    version3

--- Project 5 IntroToMachineLearning/poi_id.py
+++ Project 5 IntroToMachineLearning/poi_id.py
@@ -9,39 +9,52 @@ from sklearn.metrics import classification_report, confusion_matrix
 from copy import copy
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score 
 from random import randint
-from sklearn.grid_search import GridSearchCV
+from sklearn.grid_search import GridSearchCV 
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.cluster import KMeans
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier 
+from sklearn.cross_validation import train_test_split
 
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 from operator import itemgetter, attrgetter, methodcaller
+from tester import test_classifier
+ 
 
-
-n_test = 10 
-FEATURE_SIZE = 8
  
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+features_list = []  
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
+
 data_keys = data_dict['METTS MARK'].keys()
 
-#print data_dict['METTS MARK']
+# Get the name of features in dataset
+# The number of record
+# The number of features
+# The number of poi persons in dataset.
 print "\nThe name of features: \n", data_keys
 print "The number of records: ", len(data_dict)
 print "The length of features: ", len(data_keys) 
 print "The number of poi: ", sum([1 for key, record in data_dict.iteritems() if record['poi'] == 1])
 
-#print "\nAll person names: \n", data_dict.keys()
+ 
 
-###assist function for number of NaN in record
+### Task 2: Remove outliers
+ 
+# Assisting function to get the number of NaN in a record
 def numnan(record):
     num_nan = 0
     for rkey, rrecord in record.iteritems():
@@ -49,68 +62,84 @@ def numnan(record):
             num_nan += 1;
     return num_nan
 
+# print the record with the most NaN value
+
 print '\nThe record has the most NaN value in record and its content: \n', sorted(list([list([key, numnan(record)]) for key, record in data_dict.iteritems()]), key=itemgetter(1), reverse=True)[0]
 
-# print data_dict['LOCKHART EUGENE E']
-
-
-### Task 2: Remove outliers
+# remove two outliers: 1). 'TOTAL' record are meaningless. This record aggregate all the records information
+# 2). 'LOCKHART EUGENE E' record has 20 NaN in features except 'poi'. This record does not help for predict poi
 
 data_dict.pop('TOTAL', 0)
 data_dict.pop('LOCKHART EUGENE E', 0)
+
+# The number of record after removing outliers.
 print "\nThe number of records after remove outlier: ", len(data_dict)
+
+
+
+
 
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
 
-
-
- 
- 
-
 my_dataset = copy(data_dict)
-
 features_list = copy(data_keys)
+
+# Remove email_address feature. This feature does not relate to poi.
 features_list.remove('email_address')
-features_list.remove('poi') 
-##features_list = ['poi'] + features_list + ['poi_email_communication']
+
+# Move the poi feature as the first feature
+features_list.remove('poi')  
 features_list = ['poi'] + features_list
 
-# Combine the two features from_this_person_to_poi and from_poi_to_this_person into poi_email_communication
-# If one of the features is NaN we set the value into 0. 
-# The poi_email_communication feature value equals to the sum of  from_this_person_to_poi and from_poi_to_this_person since it is the total email communications with poi.
+ 
+#Create two new features: 
+#1). from_poi_message_rate, the ratio between the number of emails from poi email to this person and total number of emails to this person.
+#2). to_poi_message_rate, the ratio between the number of emails from this person to poi and total number of emails from this person.
 for k, record in my_dataset.iteritems():
-    if record['from_this_person_to_poi'] == 'NaN':
-        record['from_this_person_to_poi'] = 0
-    if record['from_poi_to_this_person'] == 'NaN':
-        record['from_poi_to_this_person'] = 0
-    record['poi_email_communication'] = record['from_this_person_to_poi'] + record['from_poi_to_this_person']
+    if record['to_messages'] == 'NaN' or record['from_poi_to_this_person'] == 'NaN':
+        record['from_poi_message_rate'] = 0
+    else:
+        record['from_poi_message_rate'] = float(record['from_poi_to_this_person']) / float(record['to_messages'])
+    if record['from_messages'] == 'NaN' or record['from_this_person_to_poi'] == 'NaN':
+        record['to_poi_message_rate'] = 0
+    else:
+        record['to_poi_message_rate'] = float(record['from_this_person_to_poi']) / float(record['from_messages'])
+ 
+features_list = features_list + ['from_poi_message_rate'] + ['to_poi_message_rate']
+ 
 
-print data_dict 
+# Calculate the score of the features using SelectKBest function.
+# Sort the score of features in descend order
+# Select the highest features
+# let the features_list equal to the highest k features
+# Pick the feature size k = 2, 4, 6, 8, 10, 12, 14
+# Test the best feature size by selected machine learning algorithm.
 
-temp_data = featureFormat(my_dataset, features_list)
-labels, features = targetFeatureSplit(temp_data) 
+FEATURE_SIZE = 4
+
+temp_data = featureFormat(my_dataset, features_list) 
+labels, features = targetFeatureSplit(temp_data)
 
 selector = SelectKBest(f_classif, k = FEATURE_SIZE)
 selector.fit(features, labels)
 features = selector.transform(features) 
 
-scores = np.around(selector.scores_, decimals = 3) 
+scores = np.around(selector.scores_, decimals=3) 
 score_rank = sorted(zip(features_list[1:], scores), key = itemgetter(1), reverse = True)
 
 print '\n The score of features: \n',  score_rank
-
-print '\n Selected features: '
-#features_list = ['poi'] + [record[0] for record in score_rank][:9] + ['poi_email_communication']
-#print features_list
+ 
+features_list = ['poi'] + [record[0] for record in score_rank][:FEATURE_SIZE]  
 
 
 
 ### Extract features and labels from dataset for local testing
-#data = featureFormat(my_dataset, features_list, sort_keys = True)
-#labels, features = targetFeatureSplit(data)
+
+data = featureFormat(my_dataset, features_list, sort_keys = True)
+labels, features = targetFeatureSplit(data)
  
-### Scale teh features 
+### Scale the features 
 
 from sklearn.preprocessing import MinMaxScaler
 
@@ -126,90 +155,32 @@ features = scaler.fit_transform(features)
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
 # Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-from sklearn.linear_model import LogisticRegression
-from sklearn.cluster import KMeans
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import AdaBoostClassifier 
-from sklearn.cross_validation import train_test_split
 
-
-# Evaluate the accuracy score, precision and recall score.
-def evaluation_score(labels_test, pred):
-    tp = len([(t, p)  for t, p in zip(labels_test, pred) if t == 1 and p == 1]) 
-    tn = len([(t, p)  for t, p in zip(labels_test, pred) if t == 0 and p == 0]) 
-    fp = len([(t, p)  for t, p in zip(labels_test, pred) if t == 0 and p == 1]) 
-    fn = len([(t, p)  for t, p in zip(labels_test, pred) if t == 1 and p == 0]) 
-    return round(accuracy_score(labels_test, pred), 3), 0 if (tp + fp) == 0 else tp / float(tp + fp), 0 if (tp + fn) == 0 else tp / float(tp + fn)
-    
-
-
-# random divide the feature and label set into training and testing set. 
-# Loop 1000 times
-# return the average score for each accuracy score, precision and recall score.
- 
-def evaluate_score(clf):
-    final_score = 0., 0., 0.
-    for i in range(n_test):
-        seed = randint(0, n_test)
-        features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=seed)
-        clf.fit(features_train, labels_train) 
-        score = evaluation_score(labels_test, clf.predict(features_test))
-        final_score = map(add, final_score, score)
-    return [num / n_test for num in final_score]
-
-clf = SVC()
-print "SVC accuracy score: \n", np.around(evaluate_score(clf), decimals = 3), sum(evaluate_score(clf)) 
+   
 
 clf = DecisionTreeClassifier() 
-print "DecisionTreeClassifier accuracy score: : \n", np.around(evaluate_score(clf), decimals = 3) , sum(evaluate_score(clf)) 
+test_classifier(clf, my_dataset, features_list)
 
-clf = RandomForestClassifier() 
-print "RandomForestClassifier accuracy score: \n", np.around(evaluate_score(clf), decimals = 3), sum(evaluate_score(clf))  
+#clf = RandomForestClassifier() 
+#test_classifier(clf, my_dataset, features_list)
 
-clf = AdaBoostClassifier() 
-print "AdaBoostClassifier accuracy score: \n", np.around(evaluate_score(clf), decimals = 3), sum(evaluate_score(clf))  
+#clf = AdaBoostClassifier() 
+#test_classifier(clf, my_dataset, features_list)
 
 clf = GaussianNB() 
-print "GaussianNB accuracy score: \n", np.around(evaluate_score(clf), decimals = 3), sum(evaluate_score(clf))  
+test_classifier(clf, my_dataset, features_list)  
 
-clf = KMeans(n_clusters=2) 
-print "KMeans accuracy score: \n", np.around(evaluate_score(clf), decimals = 3), sum(evaluate_score(clf))  
+#clf = KMeans(n_clusters=2) 
+#test_classifier(clf, my_dataset, features_list)  
 
 clf = LogisticRegression() 
-print "LogisticRegression accuracy score: \n", np.around(evaluate_score(clf), decimals = 3), sum(evaluate_score(clf))  
-
-# find the best parameter of KMeans and LogisticRegression
-
-param_grid_kmean = { 'tol': [1e-8,  1e-7, 1e-6, 1e-5, 1e-4, 1e-3], } 
-param_grid_logistic = {'C' : [.0001, .001, .01, .1, 1., 10.0], 'tol': [1e-8,  1e-7, 1e-6, 1e-5, 1e-4, 1e-3], 'class_weight' : [None, 'balanced'], }
+test_classifier(clf, my_dataset, features_list)   
 
 
 
-clf = GridSearchCV(LogisticRegression(), param_grid_logistic)
-clf = clf.fit(features, labels) 
-print clf.best_estimator_
+ 
 
-clf = LogisticRegression(C=0.0001, class_weight='balanced', dual=False,
-                    fit_intercept=True, intercept_scaling=1, max_iter=100,
-                    multi_class='ovr', n_jobs=1, penalty='l2', random_state=None,
-                    solver='liblinear', tol=1e-08, verbose=0, warm_start=False)
-print "LogisticRegression optimized accuracy score: \n", np.around(evaluate_score(clf), decimals = 3), sum(evaluate_score(clf))  
-
-
-clf = GaussianNB() 
-
-clf = GridSearchCV(KMeans(n_clusters=2), param_grid_kmean)
-clf = clf.fit(features, labels) 
-print clf.best_estimator_
-
-clf = KMeans(algorithm='auto', copy_x=True, init='k-means++', max_iter=300,
-    n_clusters=2, n_init=10, n_jobs=1, precompute_distances='auto',
-    random_state=None, tol=1e-05, verbose=0)
-print "KMeans optimized accuracy score: \n", np.around(evaluate_score(clf), decimals = 3), sum(evaluate_score(clf))  
-
+ 
 
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
@@ -220,9 +191,43 @@ print "KMeans optimized accuracy score: \n", np.around(evaluate_score(clf), deci
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
+
 from sklearn.cross_validation import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
+
+# find the best parameter of KMeans and LogisticRegression using GridSearchCV
+
+param_grid_kmean = { 'tol': [1e-12, 1e-11, 1e-10, 1e-9, 1e-8,  1e-7, 1e-6, 1e-5, 1e-4, 1e-3], 'max_iter': [300, 600, 1000], 'n_init': [3, 6, 10, 15],}  
+param_grid_dt = {'max_depth': [None, 2, 4, 6, 8, 10], 'min_samples_split' : [2, 4, 6, 10], 'min_samples_leaf' : [1, 2, 4, 8], }
+ 
+# Test the parameter of the selected three machine learning algorithm.
+ 
+clf = GridSearchCV(KMeans(n_clusters=2), param_grid_kmean)
+clf = clf.fit(features, labels) 
+print clf.best_estimator_
+
+clf = KMeans(algorithm='auto', copy_x=True, init='k-means++', max_iter=600,
+    n_clusters=2, n_init=6, n_jobs=1, precompute_distances='auto',
+    random_state=None, tol=1e-07, verbose=0)
+test_classifier(clf, my_dataset, features_list) 
+
+
+
+clf = GridSearchCV(DecisionTreeClassifier(), param_grid_dt)
+clf = clf.fit(features, labels) 
+print clf.best_estimator_
+
+clf = DecisionTreeClassifier(class_weight=None, criterion='gini', max_depth=8,
+        max_features=None, max_leaf_nodes=None,
+        min_impurity_split=1e-07, min_samples_leaf=1,
+        min_samples_split=2, min_weight_fraction_leaf=0.0,
+        presort=False, random_state=None, splitter='best')
+test_classifier(clf, my_dataset, features_list) 
+
+
+
+
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
